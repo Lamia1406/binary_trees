34c23625516bb78a2aaa4d62e0f7540cbfd4a731
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-11-29 08:35:37 -0800

    implement betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 /**
  * binary_tree_insert_left- inserts a node as the left-child of another node
  * @parent: pointer to the parent node of the node to create
@@ -10,7 +9,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = binary_tree_node(parent, value);
 
-	if (node == NULL)
+	if (parent == NULL)
 		return (NULL);
 	if (parent->left != NULL)
 	{
