00bcc07481ca4a052872550576e03ab662a52ea6
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-29 12:07:07 +0000

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,7 +10,7 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (0)
+		return (0);
 
 	int left_height = binary_tree_height(tree->left);
 	int right_height = binary_tree_height(tree->right);
