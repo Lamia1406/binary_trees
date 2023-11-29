d7326bba46274acb19cc7803e61e3e3f817b1fad
Droid <44108804+Effah001@users.noreply.github.com>
2023-11-29 10:09:11 +0000

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,12 +8,18 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = binary_tree_node(parent, value);
+	binary_tree_t *node;
 
-	if (node == NULL)
+	if (parent == NULL)
 		return (NULL);
+	node = binary_tree_node(parent, value);
+
 	if (parent->right != NULL)
-		binary_tree_insert_right(node, parent->right->n);
+	{
+		node->right = parent->right;
+		node->right->parent = node;
+	}
+
 	parent->right = node;
 	return (node);
 }
