fc26e17e12ab233950f566e9acb2b537b9f067e3
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-11-30 10:15:33 -0800

    correct betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -26,3 +26,46 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (0);
 }
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_height -  measures the height of a binary tree
+ * @tree: pointer to root node
+ *
+ * Return: Binary tree height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_H, right_H;
+
+	if (tree == NULL)
+		return (0);
+	left_H = binary_tree_height_traverse(tree->left);
+	right_H = binary_tree_height_traverse(tree->right);
+
+	if (left_H > right_H)
+		return (left_H);
+	else
+		return (right_H);
+}
+/**
+ * binary_tree_height_traverse -  measures the height of a binary tree
+ * @tree: pointer to root node
+ *
+ * Return: Binary tree height
+ */
+size_t binary_tree_height_traverse(const binary_tree_t *tree)
+{
+	size_t left_H, right_H;
+
+	if (tree == NULL)
+		return (0);
+	left_H = binary_tree_height_traverse(tree->left) + 1;
+	right_H = binary_tree_height_traverse(tree->right) + 1;
+
+	if (left_H >= right_H)
+		return (left_H);
+	else
+		return (right_H);
+}
