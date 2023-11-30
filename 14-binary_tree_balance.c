749b8e65f455ccaf98ffcb13e292116ceece5fa6
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-11-30 10:07:27 -0800

    correct betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,3 +19,43 @@ int binary_tree_balance(const binary_tree_t *tree)
 	r_height = binary_tree_height(tree->right);
 	return (l_height - r_height);
 }
+/**
+ * binary_tree_height -  measures the height of a binary tree
+ * @tree: pointer to root node
+ *
+ * Return: Binary tree height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_H, right_H;
+
+	if (tree == NULL)
+		return (0);
+	left_H = binary_tree_height_traverse(tree->left);
+	right_H = binary_tree_height_traverse(tree->right);
+	if (left_H > right_H)
+		return (left_H);
+	else
+		return (right_H);
+}
+/**
+ * binary_tree_height_traverse -  measures the height of a binary tree
+ * @tree: pointer to root node
+ *
+ * Return: Binary tree height
+ */
+size_t binary_tree_height_traverse(const binary_tree_t *tree)
+{
+	size_t left_H, right_H;
+
+	if (tree == NULL)
+		return (0);
+	left_H = binary_tree_height_traverse(tree->left) + 1;
+	right_H = binary_tree_height_traverse(tree->right) + 1;
+
+	if (left_H >= right_H)
+		return (left_H);
+	else
+		return (right_H);
+}
