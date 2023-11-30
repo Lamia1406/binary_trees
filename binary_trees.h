244f0afdbdfe10aed365ba0a0cc840153c379c6c
Lamia1406 <lamia.hamdi@univ-constantine2.dz>
2023-11-30 09:47:22 -0800

    add prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -39,12 +39,17 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
-
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
 /*Additional functions */
 size_t binary_tree_height_traverse(const binary_tree_t *tree);
+int check_BST(const binary_tree_t *tree, const int *min, const int *max);
+int check_comp(const binary_tree_t *tree, size_t index, size_t nodes);
 #endif /*BINARY_TREES*/
